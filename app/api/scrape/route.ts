7f8ef6df2cca740a732bd45c151c856f18906036
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-10-26 23:27:40 +0900

    feat: API 동작 조정 route.ts

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -2,19 +2,18 @@ import { NextRequest, NextResponse } from 'next/server';
 import * as cheerio from 'cheerio';
 import NodeCache from 'node-cache';
 
-const cache = new NodeCache({ stdTTL: 600 }); // 10분 동안 캐싱
+const cache = new NodeCache({ stdTTL: 1800 }); // 30분 동안 캐싱
 
-const ruliweb = 'https://bbs.ruliweb.com/best/humor_only';
-const fmkorea = 'https://www.fmkorea.com/best2';
+const ruliweb = 'https://bbs.ruliweb.com/best/humor_only/now?m=humor_only&t=default&page=';
 
 // 루리웹 스크래핑 함수
-async function scrapeRuliweb() {
-  const cachedData = cache.get(ruliweb);
+async function scrapeRuliweb(page: number) {
+  const cachedData = cache.get(ruliweb + page);
   if (cachedData) {
     return cachedData;
   }
 
-  const response = await fetch(ruliweb);
+  const response = await fetch(ruliweb + page);
   const text = await response.text();
   const $ = cheerio.load(text);
   const titles: { title: string, href: string }[] = [];
@@ -33,47 +32,17 @@ async function scrapeRuliweb() {
   });
 
   const data = { titles };
-  cache.set(ruliweb, data);
-  return data;
-}
-
-// 펨코 스크래핑 함수
-async function scrapeFmkorea() {
-  const cachedData = cache.get(fmkorea);
-  if (cachedData) {
-    return cachedData;
-  }
-
-  const response = await fetch(fmkorea);
-  const text = await response.text();
-  const $ = cheerio.load(text);
-  const titles: { title: string, href: string }[] = [];
-
-  // fmkorea의 스크래핑 로직을 추가합니다.
-  $('h3.title a.hotdeal_var8').each((index, element) => {
-    let textContent = '';
-    $(element).contents().each((_, node) => {
-      if (node.type === 'text') {
-        textContent += $(node).text().trim();
-      }
-    });
-    const href = $(element).attr('href');
-    if (textContent && href) {
-      titles.push({ title: textContent, href });
-    }
-  });
-
-  const data = { titles };
-  cache.set(fmkorea, data);
+  cache.set(ruliweb + page, data);
   return data;
 }
 
 export async function GET(req: NextRequest) {
-  try {
-    const ruliwebData = await scrapeRuliweb();
-    const fmkoreaData = await scrapeFmkorea();
+  const { searchParams } = new URL(req.url);
+  const page = parseInt(searchParams.get('page') || '1', 10);
 
-    return NextResponse.json({ ruliweb: ruliwebData, fmkorea: fmkoreaData });
+  try {
+    const ruliwebData = await scrapeRuliweb(page);
+    return NextResponse.json({ ruliweb: ruliwebData });
   } catch (error) {
     return NextResponse.json({ error: 'Failed to scrape data' }, { status: 500 });
   }
