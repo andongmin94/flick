f180ba9d23bf6647dad904ebf583d37597bab7ab
안동민 <andongmin94@gmail.com>
2024-11-01 16:39:17 +0900

    패키지 설정 정리 package.json

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -19,7 +19,7 @@ async function scrapeRuliweb(page: number) {
   const $ = cheerio.load(text);
   const titles: { title: string; href: string }[] = [];
 
-  $("div.col_9.text_wrapper a.title_wrapper").each((index, element) => {
+  $("div.col_9.text_wrapper a.deco").each((index, element) => {
     let textContent = "";
     $(element)
       .contents()
