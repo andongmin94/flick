6e7078453698ebb1de0fc9e43af5403c329ea05b
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-11-04 14:44:21 +0900

    feat: UI 동작 조정 page.tsx

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function encodeURL(url: string) {
+  return Buffer.from(url).toString('base64');
+}
+
+export function decodeURL(encoded: string) {
+  return Buffer.from(encoded, 'base64').toString('ascii');
+}
