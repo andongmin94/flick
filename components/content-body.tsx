773be70ac68c38b86df9ca9ac92685e6ba632111
안동민 <andongmin94@gmail.com>
2024-12-23 23:26:18 +0900

    UI 동작 조정 page.tsx

--- components/content-body.tsx
+++ components/content-body.tsx
@@ -6,47 +6,49 @@ import { encodeURL } from "@/lib/utils";
 
 interface ContentBodyProps {
   currentPage: number;
-  setCurrentPage: (page: number) => void;
   titles: { title: string; href: string }[];
+  isLoading: boolean;
 }
 
-export default function ContentBody({ currentPage, titles }: ContentBodyProps) {
+export default function ContentBody({ currentPage, titles, isLoading }: ContentBodyProps) {
   return (
     <AnimatePresence mode="wait">
-      <motion.div
-        key={`titles-${currentPage}`}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-      >
-        <div className="h-full p-4">
-          {titles.map((post, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: index * 0.05 }}
-            >
-              <Link href={`/${encodeURL(post.href)}`}>
-                <Button
-                  variant="ghost"
-                  className="group mb-4 w-full justify-start overflow-hidden rounded-lg border-2 border-secondary text-left transition-all duration-300 hover:border-primary hover:bg-secondary"
-                >
-                  <div className="flex w-full items-center">
-                    <div className="mr-3 rounded-full bg-primary p-2 text-primary-foreground transition-colors duration-300 group-hover:bg-secondary-foreground">
-                      <MessageCircleMore className="size-4" />
+      {!isLoading && (
+        <motion.div
+          key={`titles-${currentPage}`}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+        >
+          <div className="h-full p-4">
+            {titles.map((post, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: index * 0.05 }}
+              >
+                <Link href={`/${encodeURL(post.href)}`}>
+                  <Button
+                    variant="ghost"
+                    className="group mb-4 w-full justify-start overflow-hidden rounded-lg border-2 border-secondary text-left transition-all duration-300 hover:border-primary hover:bg-secondary"
+                  >
+                    <div className="flex w-full items-center">
+                      <div className="mr-3 rounded-full bg-primary p-2 text-primary-foreground transition-colors duration-300 group-hover:bg-secondary-foreground">
+                        <MessageCircleMore className="size-4" />
+                      </div>
+                      <div className="grow truncate">{post.title}</div>
+                      <div className="ml-2 rounded-full bg-secondary-foreground px-2 py-1 text-xs text-secondary">
+                        인기
+                      </div>
                     </div>
-                    <div className="grow truncate">{post.title}</div>
-                    <div className="ml-2 rounded-full bg-secondary-foreground px-2 py-1 text-xs text-secondary">
-                      인기
-                    </div>
-                  </div>
-                </Button>
-              </Link>
-            </motion.div>
-          ))}
-        </div>
-      </motion.div>
+                  </Button>
+                </Link>
+              </motion.div>
+            ))}
+          </div>
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 }
