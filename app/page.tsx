fd21a0adb19d30ede06d61f22e287311aa6c53f7
안동민 <110483588+andongmin94@users.noreply.github.com>
2024-12-24 00:25:38 +0900

    feat: UI 동작 조정 page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, Suspense } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Image from "next/image";
 import FlickPage from "@/components/flick-page";
@@ -62,38 +62,40 @@ export default function Component() {
   }, [generateRandomHref]); // randomHref 대신 generateRandomHref 사용
 
   return (
-    <Card className="relative rounded-none border-4 border-t-0 border-primary shadow-xl">
-      <CardHeader className="sticky top-0 z-10 bg-primary text-primary-foreground">
-        <CardTitle className="text-center text-6xl font-bold">
-          {titles.length > 0 && (
-            <Link
-              href={randomHref}
-              onMouseDown={() => setRandomHref(generateRandomHref())}
-            >
-              <Image
-                src={"/icon.png"}
-                alt={`안동민`}
-                width={80}
-                height={80}
-                className="mr-5 inline hover:cursor-pointer"
-              />
-            </Link>
-          )}
-          안동민의 플릭
-        </CardTitle>
-      </CardHeader>
-      <FlickPage
-        className="sticky top-32 z-10 bg-white"
-        currentPage={currentPage}
-        setCurrentPage={handlePageChange}
-      />
-      <CardContent className="p-0">
-      <ContentBody
+    <Suspense>
+      <Card className="relative rounded-none border-4 border-t-0 border-primary shadow-xl">
+        <CardHeader className="sticky top-0 z-10 bg-primary text-primary-foreground">
+          <CardTitle className="text-center text-6xl font-bold">
+            {titles.length > 0 && (
+              <Link
+                href={randomHref}
+                onMouseDown={() => setRandomHref(generateRandomHref())}
+              >
+                <Image
+                  src={"/icon.png"}
+                  alt={`안동민`}
+                  width={80}
+                  height={80}
+                  className="mr-5 inline hover:cursor-pointer"
+                />
+              </Link>
+            )}
+            안동민의 플릭
+          </CardTitle>
+        </CardHeader>
+        <FlickPage
+          className="sticky top-32 z-10 bg-white"
           currentPage={currentPage}
-          titles={titles}
-          isLoading={isLoading}
+          setCurrentPage={handlePageChange}
         />
-      </CardContent>
-    </Card>
+        <CardContent className="p-0">
+          <ContentBody
+            currentPage={currentPage}
+            titles={titles}
+            isLoading={isLoading}
+          />
+        </CardContent>
+      </Card>
+    </Suspense>
   );
 }
