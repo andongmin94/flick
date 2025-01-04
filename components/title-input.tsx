17d5e99ec380397f091d82862ee74adb519c33d4
안동민 <andongmin94@gmail.com>
2025-01-05 05:57:41 +0900

    f

--- components/title-input.tsx
+++ components/title-input.tsx
@@ -56,7 +56,7 @@ export function TitleInput({
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button className={cn("mb-2 mt-4 h-full", className)}>
+        <Button className={cn("mt-2 h-full", className)}>
           제목 수정하기
         </Button>
       </DialogTrigger>
