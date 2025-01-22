bed00214f9163c7dab3dbd716dda683396c1c814
안동민 <andongmin94@gmail.com>
2025-01-23 00:26:19 +0900

    구조 정리 site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "플릭",
+  name: "Flick",
   description: "리액션 쇼츠 양산 프로젝트",
   url: "https://flick.andongmin.com/",
   ogImage: "https://flick.andongmin.com/icon.png",
