fbe208b412c21d6372691c4f56227290f650cf8c
안동민 <andongmin94@gmail.com>
2025-01-22 21:05:55 +0900

    패키지 설정 정리 package.json

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "플릭",
   description: "리액션 쇼츠 양산 프로젝트",
   url: "https://flick.andongmin.com/",
-  ogImage: "https://flick.andongmin.com/flick.svg",
+  ogImage: "https://flick.andongmin.com/icon.png",
   links: {
     twitter: "https://twitter.com/andongmin94",
     github: "https://github.com/andongmin94",
