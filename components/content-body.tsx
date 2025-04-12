0b7fe35a0d9350b95ba50156db3a76463e228d21
안동민 <andongmin94@gmail.com>
2025-04-13 01:57:01 +0900

    f

--- components/content-body.tsx
+++ components/content-body.tsx
@@ -19,44 +19,47 @@ export default function ContentBody({
   return (
     <AnimatePresence mode="wait">
       {isLoading ? (
-        <div className="flex h-[79.2vh] items-center justify-center pb-40 text-5xl font-bold">
+        <div className="flex h-[75.7vh] items-center justify-center pb-10 text-4xl font-bold">
           로딩 중...
         </div>
       ) : (
-        <motion.div
-          key={`titles-${currentPage}`}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-        >
-          <div className="h-full min-h-[79.2vh] p-4">
-            {titles.map((post, index) => (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: index * 0.05 }}
-              >
-                <Link href={`/${encodeURL(post.href)}`}>
-                  <Button
-                    variant="ghost"
-                    className="group border-secondary hover:border-primary hover:bg-secondary mb-4 w-full justify-start overflow-hidden rounded-lg border-2 text-left transition-all duration-300 hover:cursor-pointer"
-                  >
-                    <div className="flex w-full items-center">
-                      <div className="bg-primary text-primary-foreground group-hover:bg-secondary-foreground mr-3 rounded-full p-2 transition-colors duration-300">
-                        <MessageCircleMore className="size-4" />
+        <div className="scrollbar-thin scrollbar-thumb-primary scrollbar-track-secondary h-[75.7vh] w-[26vw] overflow-y-auto">
+          <motion.div
+            key={`titles-${currentPage}`}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            className="h-full"
+          >
+            <div className="p-4">
+              {titles.map((post, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: index * 0.05 }}
+                >
+                  <Link href={`/${encodeURL(post.href)}`}>
+                    <Button
+                      variant="ghost"
+                      className="group border-secondary hover:border-primary hover:bg-secondary mb-4 w-full justify-start overflow-hidden rounded-lg border-2 text-left transition-all duration-300 hover:cursor-pointer"
+                    >
+                      <div className="flex w-full items-center">
+                        <div className="bg-primary text-primary-foreground group-hover:bg-secondary-foreground mr-3 rounded-full p-2 transition-colors duration-300">
+                          <MessageCircleMore className="size-4" />
+                        </div>
+                        <div className="grow truncate">{post.title}</div>
+                        <div className="bg-secondary-foreground text-secondary ml-2 rounded-full px-2 py-1 text-xs">
+                          인기
+                        </div>
                       </div>
-                      <div className="grow truncate">{post.title}</div>
-                      <div className="bg-secondary-foreground text-secondary ml-2 rounded-full px-2 py-1 text-xs">
-                        인기
-                      </div>
-                    </div>
-                  </Button>
-                </Link>
-              </motion.div>
-            ))}
-          </div>
-        </motion.div>
+                    </Button>
+                  </Link>
+                </motion.div>
+              ))}
+            </div>
+          </motion.div>
+        </div>
       )}
     </AnimatePresence>
   );
