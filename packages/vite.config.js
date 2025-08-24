caa5fca87ad8c5e624f1b615daf19610d50a1756
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-08-24 14:16:30 +0900

    build: 세부 조정 vite.config.js

--- packages/vite.config.js
+++ packages/vite.config.js
@@ -4,19 +4,11 @@ import { resolve } from "path";
 export default defineConfig({
   build: {
     outDir: "content",
-    emptyOutDir: false, // 재빌드 시 content 정리 (bundle, shorts.css 새로 생성)
-    sourcemap: false,
-    cssCodeSplit: false, // 하나의 CSS (shorts.css) 출력
+    emptyOutDir: false,
     rollupOptions: {
-      input: { bundle: resolve(__dirname, "src/entry.js") },
+      input: resolve(__dirname, "src/entry.js"),
       output: {
         entryFileNames: "bundle.js",
-        chunkFileNames: "chunks/[name]-[hash].js",
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith(".css"))
-            return "shorts.css";
-          return "assets/[name]-[hash][extname]";
-        },
       },
     },
   },
