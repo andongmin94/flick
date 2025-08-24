39cc1ce68187839763a014ed98d8ae9b0e1c2789
안동민 <andongmin94@gmail.com>
2025-08-24 19:49:12 +0900

    Update ui.js

--- packages/src/sites.js
+++ packages/src/sites.js
@@ -3,7 +3,12 @@ export const sites = [
   {
     id: "fmkorea",
     match: /https?:\/\/(www\.)?fmkorea\.com\//i,
-    articleMatch: /(\/best\/)|\/(\d+)(?:$|[?#])|index\.php\?mid=/i,
+  // 게시글 URL 기준:
+  // 1) /best/ 포함
+  // 2) 경로 끝이 /숫자 (문서 번호 직접 접근)
+  // 3) query string 에 document_srl= 존재
+  // 목록(index.php?mid=...) 만 있는 경우는 제외
+  articleMatch: /(\/best\/)|\/(\d+)(?:$|[?#])|document_srl=\d+/i,
     // video 도 수집해야 하므로 mediaelement_video 제거
     skipClosest: ".document_address",
     ruleId: "fmkorea",
